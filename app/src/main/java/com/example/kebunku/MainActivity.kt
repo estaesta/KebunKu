636880dd7b1d758cc7ff97ca99fb58ef78da821e
estaesta <ahmadforesta@gmail.com>
2022-12-02 08:07:36 +0700

    Home Fragment UI

--- app/src/main/java/com/example/kebunku/MainActivity.kt
+++ app/src/main/java/com/example/kebunku/MainActivity.kt
@@ -10,25 +10,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val rvPlant: RecyclerView = findViewById(R.id.rv_plants)
-        val list = ArrayList<Plant>()
-        val plant1 = Plant(
-            "plant 1",
-            R.drawable.broccoli
-        )
-        val plant2 = Plant(
-            "plant 2",
-            R.drawable.cabbage
-        )
-        val plant3 = Plant(
-            "plant 3",
-            R.drawable.carrot
-        )
-        list.addAll(listOf(plant1, plant2, plant3))
-        val listPlantAdapter = ListPlantAdapter(list)
-        rvPlant.adapter = listPlantAdapter
-        rvPlant.layoutManager = GridLayoutManager(this, 2)
-//        rvPlant.layoutManager = LinearLayoutManager(this)
     }
 }
--- app/src/main/java/com/example/kebunku/Plant.kt
+++ app/src/main/java/com/example/kebunku/Plant.kt
@@ -3,4 +3,5 @@ package com.example.kebunku
 data class Plant (
     var name: String = "",
     var photo: Int = 0
-    )
+    ) {
+}
